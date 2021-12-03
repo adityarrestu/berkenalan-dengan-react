19ff66c93692c052d4cb6f24486c20a26759da01
Aditya Restu Adji <74038649+adityarrestu@users.noreply.github.com>
2021-12-03 16:05:58 +0700

    pengenalan react element 3 tipe react element: tag, component, fragment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+// tipe react element:
+// 1. tag name
+// const element = <h1>Hello World!</h1>;
+
+// 2. komponen
+// const element2 = <Komponen/>;
+
+// 3. react fragment
+/* const element3 = (
+  <React.Fragment>
+    <h1>Hello</h1>
+    <h2>World!</h2>
+    <p>React is Amazing</p>
+  </React.Fragment>
+);
+*/
+
+// bisa juga menggunakan shorthand-nya
+const element3 = (
+  <>
+    <h1>Hello</h1>
+    <h2>World!</h2>
+    <p>React is Amazing</p>
+  </>
+);
+
+// mengambil root dalam HTML untuk menempatkan element dalam root
+const domElement = document.getElementById("root");
+
+// menggunakan react DOM untuk menampilkan element pada browser
+// render berisi 2 argumen, element dan letak element akan ditampilkan
+ReactDOM.render(element3, domElement);
+
+/* import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
@@ -10,3 +44,4 @@ ReactDOM.render(
   </StrictMode>,
   rootElement
 );
+*/
