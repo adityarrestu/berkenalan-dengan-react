a562aec31794628e754a2ff00c0ca03a8332f8bb
Aditya Restu Adji <74038649+adityarrestu@users.noreply.github.com>
2021-12-03 20:20:43 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 // import { StrictMode } from "react";
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App.js";
 
 // tipe react element:
 // 1. tag name
 // const element = <h1>Hello World!</h1>;
 
-// 2. komponen
+// 2. component
 // const element2 = <Komponen/>;
 
 // 3. react fragment
@@ -20,28 +21,20 @@ import ReactDOM from "react-dom";
 */
 
 // bisa juga menggunakan shorthand-nya
-const element3 = (
+/* const element3 = (
   <>
     <h1>Hello</h1>
     <h2>World!</h2>
     <p>React is Amazing</p>
   </>
 );
+*/
+
+const element = <App />;
 
 // mengambil root dalam HTML untuk menempatkan element dalam root
 const domElement = document.getElementById("root");
 
 // menggunakan react DOM untuk menampilkan element pada browser
 // render berisi 2 argumen, element dan letak element akan ditampilkan
-ReactDOM.render(element3, domElement);
-
-/* import App from "./App";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
-*/
+ReactDOM.render(element, domElement);
