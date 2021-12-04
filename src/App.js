e8314d1817cd8bc2f9a69c5210161e5cc3ece641
Aditya Restu Adji <74038649+adityarrestu@users.noreply.github.com>
2021-12-04 10:59:53 +0700

    props children Menambahkan Header.js dan penggunaan props children pada komponen Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Header from "./Header.js";
 
+// props children, konten antara komponen tag (open & close tag)
 function App(props) {
   console.log(props);
   return (
     <>
-      <h1>{props.nama}</h1>
+      <Header>{props.nama}</Header>
       <p>React is Awesome</p>
     </>
   );
 }
 
+// validasi input, prop types dengan tipe data
 App.propTypes = {
   nama: PropTypes.string.isRequired
 };
