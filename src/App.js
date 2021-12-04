519764df86de1834d20bda3080596e9257653540
Aditya Restu Adji <74038649+adityarrestu@users.noreply.github.com>
2021-12-04 11:27:54 +0700

    menggunakan useState Menambahkan useState pda App.js Mendemokan penggunaan useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Header from "./Header.js";
 
 // props children, konten antara komponen tag (open & close tag)
 function App(props) {
   console.log(props);
+
+  // menggunakan useState, count: nillai, setCount: fungsi pengubah
+  const [count, setCount] = useState(0);
+
+  // menampilkan nilai useState dan mengubahnya dengan setCount
   return (
     <>
       <Header>{props.nama}</Header>
-      <p>React is Awesome</p>
+      <p>Counter: {count}</p>
+      <button onClick={(e) => setCount(count + 1)}>Berhitung!</button>
     </>
   );
 }
