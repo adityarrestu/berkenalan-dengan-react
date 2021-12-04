ca6e07d1c0c56330b637c91199cffafc06b737a2
Aditya Restu Adji <74038649+adityarrestu@users.noreply.github.com>
2021-12-04 14:27:47 +0700

    manipulasi data  post yang ingin ditampilkan dari State Menambahkan State postId untuk digunakan pada ekspresi JS dalam alamat tujuan request fetch API berdasarkan postId dan menampilkannya menggunakan State

--- src/App.js
+++ src/App.js
@@ -7,30 +7,34 @@ function App(props) {
   console.log(props);
 
   // menggunakan useState, count: nilai, setCount: fungsi pengubah
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   // mendapatkan data untuk digunakan State
-  const [post, setPost] = useState(1);
+  const [post, setPost] = useState({});
+  // memegang data postId untuk digunakan
+  const [postId, setPostId] = useState(1);
 
   // menggunakan useEffect untuk mengakses data dari json placeholder
   useEffect(() => {
     const fetchPost = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts/1"); //request menggunakan fetch API
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${postId}`
+      ); //request menggunakan fetch API berdasarkan postId
       const post = await res.json(); // mengambil data
       console.log(post); // menampilkan pada console
       setPost(post); // mengambil post ke useState
     };
 
     fetchPost(); // memanggil perintah fetchPost
-  }, []); // menambahkan dependencies, berupa array, State atau props
+  }, [postId]); // menambahkan dependencies, berupa array, State atau props
 
   // menampilkan nilai useState
   return (
     <>
       <Header>{props.nama}</Header>
       <p>React is Awesome</p>
-      <p>Counter: {count}</p>
-      <button onClick={(e) => setCount(count + 1)}>Berhitung!</button>
+      <p>Post ID: {postId}</p>
+      <button onClick={(e) => setPostId(postId + 1)}>Next Post</button>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
     </>
