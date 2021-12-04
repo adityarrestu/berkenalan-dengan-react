dee248dd542dff9484d698aa92f4a97c3ce98dbe
Aditya Restu Adji <74038649+adityarrestu@users.noreply.github.com>
2021-12-04 13:57:11 +0700

    penggunaan useEffect Menambahkan useEffect, membuat request menggunakan fetch API dan menampilkan data pada console

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Header from "./Header.js";
 
@@ -9,10 +9,22 @@ function App(props) {
   // menggunakan useState, count: nillai, setCount: fungsi pengubah
   const [count, setCount] = useState(0);
 
+  // menggunakan useEffect untuk mengakses data dari json placeholder
+  useEffect(() => {
+    const fetchPost = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts/1"); //request menggunakan fetch API
+      const post = await res.json(); // mengambil data
+      console.log(post); // menampilkan pada console
+    };
+
+    fetchPost(); // memanggil perintah fetchPost
+  });
+
   // menampilkan nilai useState dan mengubahnya dengan setCount
   return (
     <>
       <Header>{props.nama}</Header>
+      <p>React is Awesome</p>
       <p>Counter: {count}</p>
       <button onClick={(e) => setCount(count + 1)}>Berhitung!</button>
     </>
